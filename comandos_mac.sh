a339fe159889936a916da018829c1939e856309f
fabiowillianrp <fabiowillian@gmail.com>
2026-08-05 16:10:28 -0300

    Add comment for CPU unlock script in comandos_mac.sh
    Add comment to explain CPU unlock script functionality

--- comandos_mac.sh
+++ comandos_mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Destrava o bloqueio do limite de 800mhz do processador do macbook air 2011
 # Comando manual:
 # sudo modprobe msr && val=$(sudo rdmsr 0x1FC); sudo wrmsr -a 0x1FC 0x$(printf '%x' $(( 0x$val & ~1 )))
 
